98b0a490f234516d9069c83a0cac46ae75210fd4
Tpou <tienphn@gmail.com>
2022-05-08 11:52:26 +0700

    removed test

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class test(models.Model):
-    userid = models.IntegerField(primary_key=True)
-    username = models.CharField(max_length=10)
+
