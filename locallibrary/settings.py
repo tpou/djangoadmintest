b1c441ede84d64173ba1a0fece4812dff46549d5
Tpou <tienphn@gmail.com>
2022-05-21 20:02:16 +0700

    mod-db

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catalog',
+        'USER': 'postgres',
+        'PASSWORD': 'Tien30504283',
+        'host': 'localhost'
     }
 }
 
